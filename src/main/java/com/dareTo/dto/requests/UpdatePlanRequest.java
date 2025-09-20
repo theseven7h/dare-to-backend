d80448c69e4635b183214fdfd19693fa8da4f561
theseven7h <jamestauri123@gmail.com>
2025-09-20 13:49:38 +0100

    update plan request

--- src/main/java/com/dareTo/dto/requests/UpdatePlanRequest.java
+++ src/main/java/com/dareTo/dto/requests/UpdatePlanRequest.java
@@ -1,8 +1,13 @@
 package com.dareTo.dto.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class UpdatePlanRequest {
@@ -13,4 +18,12 @@ public class UpdatePlanRequest {
     @Size(min = 10, message = "Description must contain at least 10 characters")
     @Pattern(regexp = ".*\\S.*", message = "Description name must not be only whitespace")
     private String description;
+
+    @FutureOrPresent(message = "Start date cannot be in the past")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @FutureOrPresent(message = "Completion date cannot be in the past")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
 }
