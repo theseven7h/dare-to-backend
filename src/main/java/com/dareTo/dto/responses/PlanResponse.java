d5ae19f1054df7c8f193dbf71e1ba96fd691e056
theseven7h <jamestauri123@gmail.com>
2025-09-20 13:48:53 +0100

    plan response

--- src/main/java/com/dareTo/dto/responses/PlanResponse.java
+++ src/main/java/com/dareTo/dto/responses/PlanResponse.java
@@ -3,6 +3,7 @@ package com.dareTo.dto.responses;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,6 +13,12 @@ public class PlanResponse {
     private String description;
     private boolean isCompleted;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime lastModified;
 }
