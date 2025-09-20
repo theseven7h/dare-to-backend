4cec51f7db84a766d3fdb99f40d9a2409d91aee3
theseven7h <jamestauri123@gmail.com>
2025-09-20 13:50:21 +0100

    global handler

--- src/main/java/com/dareTo/utils/GlobalHandlerErrorHandling.java
+++ src/main/java/com/dareTo/utils/GlobalHandlerErrorHandling.java
@@ -1,9 +1,6 @@
 package com.dareTo.utils;
 
-import com.dareTo.exceptions.EmptyUpdateDetailsException;
-import com.dareTo.exceptions.IncorrectUsernameOrPasswordException;
-import com.dareTo.exceptions.UserNotFoundException;
-import com.dareTo.exceptions.UsernameTakenException;
+import com.dareTo.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +18,8 @@ public class GlobalHandlerErrorHandling {
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
-            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            errors.put("field",fieldError.getField());
+            errors.put("message",fieldError.getDefaultMessage());
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
@@ -28,14 +27,15 @@ public class GlobalHandlerErrorHandling {
     @ExceptionHandler(UsernameTakenException.class)
     public ResponseEntity<Map<String, String>> handleUsernameTakenException(UsernameTakenException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("username", ex.getMessage());
+        errors.put("field", "username taken");
+        errors.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<Map<String, String>> handleServletException() {
         Map<String, String> errors = new HashMap<>();
-        errors.put("error", "Unauthorized");
+        errors.put("field", "Unauthorized");
         errors.put("message", "Please login again");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
     }
@@ -43,21 +43,46 @@ public class GlobalHandlerErrorHandling {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("username", ex.getMessage());
+        errors.put("field", "user not found");
+        errors.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
     }
 
     @ExceptionHandler(EmptyUpdateDetailsException.class)
     public ResponseEntity<Map<String, String>> handleEmptyUpdateDetailsException(EmptyUpdateDetailsException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("title,description", ex.getMessage());
+        errors.put("field", "empty update");
+        errors.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
     @ExceptionHandler(IncorrectUsernameOrPasswordException.class)
     public ResponseEntity<Map<String, String>> handleIncorrectUsernameOrPasswordException(IncorrectUsernameOrPasswordException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("username, password", ex.getMessage());
+        errors.put("field", "username,password");
+        errors.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<Map<String, String>> handleDateTimeParseException(DateTimeParseException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("field", "dateTime");
+        errors.put("message", "Date format is invalid (use yyyy-MM-dd)");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(PlanNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePlanNotFoundException(PlanNotFoundException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("field", "plan");
+        errors.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
+    }
+
+    @ExceptionHandler(EmailTakenException.class)
+    public ResponseEntity<Map<String, String>> handleEmailTakenException(EmailTakenException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new HashMap<>(Map.of("field", "email taken", "message", ex.getMessage())));
     }
 }
