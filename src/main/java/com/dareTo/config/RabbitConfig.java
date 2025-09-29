9a0bfd0abd053b901e7c18663aa274e40bb36c34
theseven7h <jamestauri123@gmail.com>
2025-09-29 20:09:24 +0100

    rabbit config

---
+++ src/main/java/com/dareTo/config/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.dareTo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String EXCHANGE = "emailExchange";
+    public static final String QUEUE = "emailQueue";
+    public static final String ROUTING_KEY = "emailRoutingKey";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+}
