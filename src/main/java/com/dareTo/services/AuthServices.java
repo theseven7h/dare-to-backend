0247e9b2ae231356e1d2bb3fc0947e0cdad48622
theseven7h <jamestauri123@gmail.com>
2025-10-02 10:20:16 +0100

    Modified services

--- src/main/java/com/dareTo/services/AuthServices.java
+++ src/main/java/com/dareTo/services/AuthServices.java
@@ -1,11 +1,16 @@
 package com.dareTo.services;
 
 import com.dareTo.dto.requests.LoginRequest;
+import com.dareTo.dto.requests.ResetRequest;
 import com.dareTo.dto.requests.UserRequest;
 import com.dareTo.dto.responses.LoginDto;
+import com.dareTo.dto.responses.ResetPasswordResponse;
 import com.dareTo.dto.responses.UserResponse;
 
 public interface AuthServices {
     UserResponse registerUser(UserRequest userRequest);
     LoginDto loginUser(LoginRequest userRequest);
+    ResetPasswordResponse forgotPassword(String email);
+    boolean resetPassword(ResetRequest resetRequest);
+
 }
--- src/main/java/com/dareTo/services/impl/AuthServicesImpl.java
+++ src/main/java/com/dareTo/services/impl/AuthServicesImpl.java
@@ -1,29 +1,44 @@
 package com.dareTo.services.impl;
 
 import com.dareTo.config.JwtManager;
+import com.dareTo.data.models.ResetToken;
 import com.dareTo.data.models.User;
+import com.dareTo.data.repositories.ResetTokenRepo;
 import com.dareTo.data.repositories.UserRepo;
 import com.dareTo.dto.requests.LoginRequest;
+import com.dareTo.dto.requests.ResetRequest;
 import com.dareTo.dto.requests.UserRequest;
 import com.dareTo.dto.responses.LoginDto;
+import com.dareTo.dto.responses.ResetPasswordResponse;
 import com.dareTo.dto.responses.UserResponse;
-import com.dareTo.exceptions.EmailTakenException;
-import com.dareTo.exceptions.IncorrectUsernameOrPasswordException;
-import com.dareTo.exceptions.UsernameTakenException;
+import com.dareTo.exceptions.*;
 import com.dareTo.services.AuthServices;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import static com.dareTo.utils.Mapper.*;
+import static com.dareTo.utils.Password.hashPassword;
 import static com.dareTo.utils.Password.matches;
 
+@Slf4j
 @Service
 public class AuthServicesImpl implements AuthServices {
     @Autowired
     private UserRepo userRepo;
 
     @Autowired
+    private ResetTokenRepo resetTokenRepo;
+
+    @Autowired
     private JwtManager jwtManager;
+
+    @Autowired
+    private EmailTypeServices  emailTypeServices;
+
 
 //    @Autowired
 //    private SessionManager sessionManager;
@@ -38,6 +53,8 @@ public class AuthServicesImpl implements AuthServices {
         }
         User user = mapToUser(request);
         User savedUser = userRepo.save(user);
+
+        emailTypeServices.sendWelcomeEmail(request);
         return mapToUserResponse(savedUser);
     }
 
@@ -57,4 +74,45 @@ public class AuthServicesImpl implements AuthServices {
         response.setToken(token);
         return response;
     }
+
+    @Override
+    public ResetPasswordResponse forgotPassword(String email) {
+        log.info("Forgot password reset request");
+        User user = userRepo.findByEmail(email).orElseThrow(() -> new EmailNotFoundException("No user with this email"));
+
+        ResetToken resetToken = resetTokenRepo.findByUserId(user.getId());
+        if(resetToken != null) {
+            resetTokenRepo.delete(resetToken);
+        }
+        ResetToken newResetToken = getResetToken(user);
+        ResetToken savedResetToken = resetTokenRepo.save(newResetToken);
+
+        emailTypeServices.sendForgotPasswordEmail(email, newResetToken.getToken());
+        return mapToResetPasswordResponse(savedResetToken);
+    }
+
+    @Override
+    public boolean resetPassword(ResetRequest resetRequest) {
+        User user = userRepo.findByEmail(resetRequest.getEmail()).orElseThrow(() -> new EmailNotFoundException("No user with this email"));
+        ResetToken resetToken = resetTokenRepo.findByUserId(user.getId());
+        if(resetToken == null || resetToken.getExpiryTime().isBefore(LocalDateTime.now())) {
+            resetTokenRepo.delete(resetToken);
+            throw new InvalidTokenException("Token is invalid or expired");
+        }
+        user.setPassword(hashPassword(resetRequest.getPassword()));
+        userRepo.save(user);
+        resetTokenRepo.delete(resetToken);
+        return true;
+    }
+
+
+    private ResetToken getResetToken(User user) {
+        UUID token = UUID.randomUUID();
+        ResetToken resetToken = new ResetToken();
+        resetToken.setUserId(user.getId());
+        resetToken.setToken(token.toString());
+        resetToken.setExpiryTime(LocalDateTime.now().plusMinutes(15));
+        return resetToken;
+    }
+
 }
